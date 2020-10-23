32db4241807932d14fc54ed72e07650bafcd6ba6
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-10-23 11:56:36 +0200

    setting up test environment

--- backend/app/db/migrations/versions/d514b9a4f8a6_create_main_tables.py
+++ backend/app/db/migrations/versions/d514b9a4f8a6_create_main_tables.py
@@ -3,7 +3,7 @@ Revision ID: d514b9a4f8a6
 Revises: 
 Create Date: 2020-10-23 09:22:02.619057
 """
-
+from typing import Tuple
 from alembic import op
 import sqlalchemy as sa
 import geoalchemy2 as geo
@@ -14,6 +14,56 @@ down_revision = None
 branch_labels = None
 depends_on = None
 
+def create_updated_at_trigger() -> None:
+    op.execute(
+        """
+        CREATE OR REPLACE FUNCTION update_updated_at_column()
+            RETURNS TRIGGER AS
+        $$
+        BEGIN
+            NEW.updated_at = now();
+            RETURN NEW;
+        END;
+        $$ language 'plpgsql';
+        """
+    )
+def timestamps() -> Tuple[sa.Column, sa.Column]:
+    return (
+        sa.Column(
+            "created_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+        ),
+    )
+def create_users_table() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("username", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("email", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("email_verified", sa.Boolean, nullable=False, server_default="False"),
+        sa.Column("salt", sa.Text, nullable=False),
+        sa.Column("password", sa.Text, nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False, server_default="True"),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False, server_default="False"),
+        *timestamps(),
+    )
+    op.execute(
+        """
+        CREATE TRIGGER update_user_modtime
+            BEFORE UPDATE
+            ON users
+            FOR EACH ROW
+        EXECUTE PROCEDURE update_updated_at_column();
+        """
+    )
 def create_areas_monitored_table() -> None:
     op.create_table(
         "areas_monitored",
@@ -21,10 +71,24 @@ def create_areas_monitored_table() -> None:
         sa.Column("user_id", sa.Integer, nullable=False, index=True),
         sa.Column("coordinates", geo.Geometry('POINT'), nullable=True),
         sa.Column("notifications", sa.Boolean(), nullable=False, server_default="False"),
+        *timestamps(),
+    )
+    op.execute(
+        """
+        CREATE TRIGGER update_areas_monitoring_modtime
+            BEFORE UPDATE
+            ON areas_monitored
+            FOR EACH ROW
+        EXECUTE PROCEDURE update_areas_monitoring_modtime();
+        """
     )
 
 def upgrade() -> None:
+    create_updated_at_trigger()
+    create_users_table()
     create_areas_monitored_table()
 
 def downgrade() -> None:
+    op.drop_table("users")
     op.drop_table("areas_monitored")
+    op.execute("DROP FUNCTION update_updated_at_column")
