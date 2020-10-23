82e78928259d4010d70aa57fbffc4240bd808614
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-10-23 12:19:26 +0200

    user repository implementation

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 from app.api.routes.areas_monitored import router as areas_router
-
+from app.api.routes.users import router as users_router
 
 
 router = APIRouter()
 
 
 router.include_router(areas_router, prefix="/areas", tags=["areas"])
+router.include_router(users_router, prefix="/users", tags=["users"])
