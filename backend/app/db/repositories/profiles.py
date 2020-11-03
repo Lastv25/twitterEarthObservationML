03323492a1d3d2e0e888c84609c34bb529d450b4
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-11-03 15:13:44 +0100

    setting up authentification verification for the get route of profiles

--- backend/app/db/repositories/profiles.py
+++ backend/app/db/repositories/profiles.py
@@ -11,7 +11,23 @@ GET_PROFILE_BY_USER_ID_QUERY = """
     FROM profiles
     WHERE user_id = :user_id;
 """
-
+GET_PROFILE_BY_USERNAME_QUERY = """
+    SELECT p.id,
+           u.email AS email,
+           u.username AS username,
+           full_name,
+           phone_number,
+           bio,
+           image,
+           collections,
+           user_id,
+           p.created_at,
+           p.updated_at
+    FROM profiles p
+        INNER JOIN users u 
+        ON p.user_id = u.id
+    WHERE user_id = (SELECT id FROM users WHERE username = :username);
+"""
 class ProfilesRepository(BaseRepository):
 
     async def create_profile_for_user(self, *, profile_create: ProfileCreate) -> ProfileInDB:
@@ -20,6 +36,12 @@ class ProfilesRepository(BaseRepository):
 
     async def get_profile_by_user_id(self, *, user_id: int) -> ProfileInDB:
         profile_record = await self.db.fetch_one(query=GET_PROFILE_BY_USER_ID_QUERY, values={"user_id": user_id})
+        if not profile_record:
+            return None
+        return ProfileInDB(**profile_record)
+
+    async def get_profile_by_username(self, *, username: str) -> ProfileInDB:
+        profile_record = await self.db.fetch_one(query=GET_PROFILE_BY_USERNAME_QUERY, values={"username": username})
         if not profile_record:
             return None
         return ProfileInDB(**profile_record)
