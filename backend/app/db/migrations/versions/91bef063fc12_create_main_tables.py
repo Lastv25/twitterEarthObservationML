0b3ba7a17b468167b87a9c032685e9f7a963c3a0
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-11-03 11:14:21 +0100

    setting up postgres connection and alembic setup

---
+++ backend/app/db/migrations/versions/91bef063fc12_create_main_tables.py
@@ -0,0 +1,21 @@
+"""create_main_tables
+Revision ID: 91bef063fc12
+Revises: 
+Create Date: 2020-11-03 10:12:29.828454
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic
+revision = '91bef063fc12'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
