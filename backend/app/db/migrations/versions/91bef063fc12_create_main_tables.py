202464dbe88af5b5e9385955627e11e1424d803a
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-11-03 11:47:51 +0100

    creation of the user table and testing it with pytest

--- backend/app/db/migrations/versions/91bef063fc12_create_main_tables.py
+++ backend/app/db/migrations/versions/91bef063fc12_create_main_tables.py
@@ -6,7 +6,7 @@ Create Date: 2020-11-03 10:12:29.828454
 
 from alembic import op
 import sqlalchemy as sa
-
+from typing import Tuple
 
 # revision identifiers, used by Alembic
 revision = '91bef063fc12'
@@ -14,8 +14,65 @@ down_revision = None
 branch_labels = None
 depends_on = None
 
+def create_updated_at_trigger() -> None:
+    op.execute(
+        """
+        CREATE OR REPLACE FUNCTION update_updated_at_column()
+            RETURNS TRIGGER AS
+        $$
+        BEGIN
+            NEW.updated_at = now();
+            RETURN NEW;
+        END;
+        $$ language 'plpgsql';
+        """
+    )
+
+
+def timestamps() -> Tuple[sa.Column, sa.Column]:
+    return (
+        sa.Column(
+            "created_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+        ),
+    )
+
+
+def create_users_table() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("username", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("email", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("email_verified", sa.Boolean, nullable=False, server_default="False"),
+        sa.Column("salt", sa.Text, nullable=False),
+        sa.Column("password", sa.Text, nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False, server_default="True"),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False, server_default="False"),
+        *timestamps(),
+    )
+    op.execute(
+        """
+        CREATE TRIGGER update_user_modtime
+            BEFORE UPDATE
+            ON users
+            FOR EACH ROW
+        EXECUTE PROCEDURE update_updated_at_column();
+        """
+    )
+
 def upgrade() -> None:
-    pass
+    create_updated_at_trigger()
+    create_users_table()
 
 def downgrade() -> None:
-    pass
+    op.drop_table("users")
+    op.execute("DROP FUNCTION update_updated_at_column")
