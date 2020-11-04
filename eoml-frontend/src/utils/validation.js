1cb6105854ed804067d4cbee609bbef5cf6faffe
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-11-04 15:33:02 +0100

    implementation of registration form and validation functions

--- eoml-frontend/src/utils/validation.js
+++ eoml-frontend/src/utils/validation.js
@@ -8,9 +8,8 @@ export function validateEmail(text) {
   return text?.indexOf("@") !== -1
 }
 
-
 /**
- * Ensures password is at least a certain length
+ * Ensures password is of at least a certain length
  *
  * @param {String} password - password to be validated
  * @param {Integer} length - length password must be as long as
@@ -19,7 +18,18 @@ export function validateEmail(text) {
 export function validatePassword(password, length = 7) {
   return password?.length >= length
 }
+
+/**
+ * Ensures a username consists of only letters, numbers, underscores, and dashes
+ *
+ * @param {String} username - username to be validated
+ * @return {Boolean}
+ */
+export function validateUsername(username) {
+  return /^[a-zA-Z0-9_-]+$/.test(username)
+}
 export default {
   email: validateEmail,
   password: validatePassword,
+  username: validateUsername
 }
