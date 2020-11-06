ca41027f072a06cb9b013b411026f0f0c5d85937
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-11-06 12:07:02 +0100

    SQL query for collection creation

--- backend/app/db/repositories/collections.py
+++ backend/app/db/repositories/collections.py
@@ -1,4 +1,19 @@
 from app.db.repositories.base import BaseRepository
+from app.models.profile import ProfileCreate, ProfileUpdate, ProfileInDB
+from app.models.user import UserInDB
+from app.models.collections import CollectionCreate, CollectionInDB
+
+
+CREATE_COLLECTION_FOR_USER_QUERY = """
+    INSERT INTO collections (full_name, disaster, notification, aoi,parameters, user_id)
+    VALUES (:full_name, :disaster, :notification, :aoi,parameters, :user_id)
+    RETURNING id, full_name, disaster, notification, aoi,parameters, user_id, created_at, updated_at;
+"""
+
+
 
 class CollectionsRepository(BaseRepository):
-    pass
+
+    async def create_collection_for_user(self, *, collection_create: CollectionCreate) -> CollectionInDB:
+        created_collection = await self.db.fetch_one(query=CREATE_COLLECTION_FOR_USER_QUERY, values=collection_create.dict())
+        return created_collection
