1ed00de87378560a559257c92398ab69e107297a
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-11-06 13:53:46 +0100

    updated collectionbase with aoi validator

--- backend/app/models/collections.py
+++ backend/app/models/collections.py
@@ -1,11 +1,23 @@
 from typing import Optional
+from pydantic import validator
 from app.models.core import DateTimeModelMixin, IDModelMixin, CoreModel
+from shapely.wkt import loads
+
 
 class CollectionBase(CoreModel):
     full_name: Optional[str]
     disaster: Optional[str]
     notification: bool = False
+    aoi: Optional[str]
     parameters: Optional[str]
+
+    @validator('aoi')
+    def polygon_validator(cls,v):
+        try:
+            area = loads(v)
+        except:
+            raise ValueError('Area of interest is not a polygon')
+        return area
 
 class CollectionCreate(CollectionBase):
     """
