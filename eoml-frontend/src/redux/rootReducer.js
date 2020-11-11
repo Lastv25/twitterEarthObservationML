858dd23f9b11b071ad5fa2b2f88cc977b33ff49a
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-11-11 11:50:45 +0100

    adding collection reducer to root reducer

--- eoml-frontend/src/redux/rootReducer.js
+++ eoml-frontend/src/redux/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux"
 import authReducer from "./auth"
+import collectionsReducer from "./collections"
 
 
 const rootReducer = combineReducers({
   auth: authReducer,
   // other reducers will go here later
+  coll: collectionsReducer,
 })
 export default rootReducer
