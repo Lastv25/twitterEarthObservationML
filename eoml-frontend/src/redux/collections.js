b6cd4bebf060fc42d87eee37cda316535215f680
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-11-11 11:38:20 +0100

    creation of redux file for collections handling

---
+++ eoml-frontend/src/redux/collections.js
@@ -0,0 +1,7 @@
+import initialState from "./initialState"
+import apiClient from "../services/apiClient"
+
+
+export const CREATE_COLLECTION_JOB = "@@collections/CREATE_COLLECTION_JOB"
+export const CREATE_COLLECTION_JOB_SUCCESS = "@@collections/CREATE_COLLECTION_JOB_SUCCESS"
+export const CREATE_COLLECTION_JOB_FAILURE = "@@collections/CREATE_COLLECTION_JOB_FAILURE"
