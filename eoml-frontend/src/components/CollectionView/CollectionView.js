e13829ddb5ad56310025b30a151cfa2aaf9f878a
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-11-18 17:19:22 +0100

    setting up blanc collection page

--- eoml-frontend/src/components/CollectionView/CollectionView.js
+++ eoml-frontend/src/components/CollectionView/CollectionView.js
@@ -1,5 +1,37 @@
-import React from "react"
+import React from 'react';
 
-export default function NotFoundPage(){
-  return <h1>Collection Modif and delete Page</h1>
-}
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageContentBody,
+  EuiPageContentHeader,
+  EuiPageContentHeaderSection,
+  EuiPageHeader,
+  EuiPageHeaderSection,
+  EuiTitle,
+} from '@elastic/eui';
+
+export default () => (
+  <EuiPage>
+    <EuiPageBody component="div">
+      <EuiPageHeader>
+        <EuiPageHeaderSection>
+          <EuiTitle size="l">
+            <h1>Collection Modif and Delete</h1>
+          </EuiTitle>
+        </EuiPageHeaderSection>
+      </EuiPageHeader>
+      <EuiPageContent>
+        <EuiPageContentHeader>
+          <EuiPageContentHeaderSection>
+            <EuiTitle>
+              <h2>Content title</h2>
+            </EuiTitle>
+          </EuiPageContentHeaderSection>
+        </EuiPageContentHeader>
+        <EuiPageContentBody>Content body</EuiPageContentBody>
+      </EuiPageContent>
+    </EuiPageBody>
+  </EuiPage>
+);
