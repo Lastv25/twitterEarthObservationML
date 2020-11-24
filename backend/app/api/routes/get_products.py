ddb49e72879e1e7a9923872998a47cdaad310524
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-11-24 11:15:57 +0100

    sentinel 1 query implemented

--- backend/app/api/routes/get_products.py
+++ backend/app/api/routes/get_products.py
@@ -7,8 +7,36 @@ from app.models.user import UserInDB
 from starlette.status import HTTP_201_CREATED
 from typing import List
 import json
+import requests
 
 router = APIRouter()
+
+def get_scihub_url(json_parameters):
+    base_url = 'https://scihub.copernicus.eu/dhus/search?q='
+
+    if json_parameters['ingestion_parameter'][0]:
+        ingestion_query = 'ingestiondate: [' + str(json_parameters['ingestion_parameter'][1]) + ' TO ' +\
+                          str(json_parameters['ingestion_parameter'][2]) + ']'
+        res = base_url+'('+ingestion_query+')'
+    if json_parameters['sensing_parameter'][0]:
+        sensing_query = 'beginposition: [' + str(json_parameters['sensing_parameter'][1]) + ' TO ' + \
+                          str(json_parameters['sensing_parameter'][2]) + ']'
+        res = base_url + '(' + sensing_query + ')'
+    if json_parameters['mission1'][0]:
+        sentinel1_query = 'platformname:Sentinel-1'
+        if len(json_parameters['mission1'][1]) > 0:
+            sentinel1_query += ''
+        if len(json_parameters['mission1'][2]) > 0:
+            sentinel1_query += ' AND producttype:'+json_parameters['mission1'][2]
+        if len(json_parameters['mission1'][3]) > 0:
+            sentinel1_query += ' AND polarisationmode:'+json_parameters['mission1'][3]
+        if len(json_parameters['mission1'][4]) > 0:
+            sentinel1_query += ' AND sensoroperationalmode:'+json_parameters['mission1'][4]
+        if len(json_parameters['mission1'][5]) > 0:
+            sentinel1_query += ' AND relativeorbitnumber:'+json_parameters['mission1'][5]
+        res = base_url + '(' + sentinel1_query + ')'
+
+    return res
 
 
 @router.get("/products/{collection_id}", response_model=List[str], name="collections:get-products-from-parameter")
@@ -23,4 +51,8 @@ async def get_collections_by_id(
     scihub_parameters = parameters['platform']['scihub']
     egeos_parameters = parameters['platform']['egeos']
 
-    return [str(scihub_parameters), str(egeos_parameters)]
+    r = requests.get(get_scihub_url(scihub_parameters), auth=('Lastv', 'bonjour_moi'))
+
+
+
+    return [r.text]
