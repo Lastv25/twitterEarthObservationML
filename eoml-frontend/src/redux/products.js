e9ad51466cd98a727d0227a67d21d26bc9e0b6c0
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2021-01-08 10:36:42 +0100

    adding redux action to fetch product list

--- eoml-frontend/src/redux/products.js
+++ eoml-frontend/src/redux/products.js
@@ -1,6 +1,48 @@
 import initialState from "./initialState"
 import apiClient from "../services/apiClient"
 
+
+export const FETCH_PRODUCTS = "@@collections/FETCH_PRODUCTS"
+export const FETCH_PRODUCTS_SUCCESS = "@@collections/FETCH_PRODUCTS_SUCCESS"
+export const FETCH_PRODUCTS_FAILURE = "@@collections/FETCH_PRODUCTS_FAILURE"
+
 export default function productsReducer(state = initialState.products, action = {}) {
+    switch (action.type) {
+        case FETCH_PRODUCTS:
+          return {
+            ...state,
+            isLoading: true
+          }
+        case FETCH_PRODUCTS_SUCCESS:
+          return {
+            ...state,
+            isLoading: false,
+            error: null,
+            data: action.data
+          }
+        case FETCH_PRODUCTS_FAILURE:
+          return {
+            ...state,
+            isLoading: false,
+            error: action.error,
+            data: {}
+          }
+    }
 }
 export const Actions = {}
+
+Actions.fetchCollections = ({ collection_id }) => {
+  return apiClient({
+    url: `/products/products/${collection_id}`,
+    method: `GET`,
+    types: {
+      REQUEST: FETCH_PRODUCTS,
+      SUCCESS: FETCH_PRODUCTS_SUCCESS,
+      FAILURE: FETCH_PRODUCTS_FAILURE,
+    },
+    options: {
+      data: {},
+      params: {},
+    },
+  })
+}
